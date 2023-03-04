39d20ad4aceef191b24129111397fd2440bb36db
Jonathan Melnyk Mendes <jonathan.melnyk.mendes@360-systems.com>
2023-03-04 16:27:34 +0000

    feat: added base template

---
+++ src/ReadList.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReadList.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
